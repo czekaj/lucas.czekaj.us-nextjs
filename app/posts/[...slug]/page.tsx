040196b412b995cef78642fd5ed13457d3f62b17
Lucas Czekaj <lukasz@czekaj.us>
2024-02-25 16:13:25 -0800

    Fix error

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -2,13 +2,6 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypeHighlight from "rehype-highlight";
 import "@/styles/highlight-js/styles/github-dark.css";
 
-const options = {
-  mdxOptions: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypeHighlight],
-  },
-};
-
 const fs = require("fs");
 const path = require("path");
 const matter = require("gray-matter");
@@ -32,13 +25,19 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
   const slug = decodeURI(params.slug.join("/"));
   const postData = getPostData(slug);
   const postContent = postData.content;
+  const options = {
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [rehypeHighlight],
+    },
+  };
   return (
     <div className="flex flex-col items-center">
       <div className="max-w-5xl m-10 p-10 bg-gray-900">
         <h1 className="text-5xl my-5">{postData.metadata.title}</h1>
         <hr className="mx-4 my-16" />
         <article className="prose">
-          <MDXRemote source={postContent} options={options} />
+          <MDXRemote source={postContent} options={options as any} />
         </article>
       </div>
     </div>
