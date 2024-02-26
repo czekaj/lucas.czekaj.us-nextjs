aa41145354222161d26c5867b7c191c8fb0161de
Lucas Czekaj <lukasz@czekaj.us>
2024-02-25 16:04:04 -0800

    Add code syntax highlighting

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,4 +1,13 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import rehypeHighlight from "rehype-highlight";
+import "@/styles/highlight-js/styles/github-dark.css";
+
+const options = {
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeHighlight],
+  },
+};
 
 const fs = require("fs");
 const path = require("path");
@@ -22,7 +31,6 @@ const getPostData = (slug: String) => {
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const slug = decodeURI(params.slug.join("/"));
   const postData = getPostData(slug);
-  console.log(postData);
   const postContent = postData.content;
   return (
     <div className="flex flex-col items-center">
@@ -30,7 +38,7 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
         <h1 className="text-5xl my-5">{postData.metadata.title}</h1>
         <hr className="mx-4 my-16" />
         <article className="prose">
-          <MDXRemote source={postContent} />
+          <MDXRemote source={postContent} options={options} />
         </article>
       </div>
     </div>
