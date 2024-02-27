8f1e7108d3cec327371653beac6eb0433cbbf531
Lucas Czekaj <lukasz@czekaj.us>
2024-02-27 11:44:01 -0800

    Update basePath for Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,14 @@ const mdx = withMDX({
   extension: /\.mdx?$/,
 });
 
+const basePath =
+  process.env.GITHUB_PAGES === "true" ? __dirname.split("/").pop() : "";
+
 const nextConfig = {
   ...mdx,
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
   output: "export",
+  basePath,
   images: { unoptimized: true },
 };
 
