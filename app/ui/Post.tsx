0112143aea1bcadfefff5825faf58c426a82fb80
Lucas Czekaj <lukasz@czekaj.us>
2024-02-27 09:38:47 -0800

    Adjust styles

--- app/ui/Post.tsx
+++ app/ui/Post.tsx
@@ -22,16 +22,16 @@ const options = {
 
 const Post: NextPage<PostProps> = ({ title, content, date, lastUpdated }) => {
   return (
-    <article>
-      <section id="header">
+    <article className="">
+      <section id="post-header" className="w-full">
         <h1 className="text-5xl my-5 text-center">{title}</h1>
         <h2 className="text-center text-gray-400">{date}</h2>
-        <hr className="mx-4 mt-12 mb-16 border-gray-600" />
+        <hr className="mt-12 mb-16 mx-20 border-gray-600" />
       </section>
-      <section id="body" className="prose m-8 text-gray-200">
+      <section id="post-body" className="prose my-16 mx-24 text-gray-200">
         <MDXRemote source={content} options={options as any} />
       </section>
-      <section id="footer" className="text-center">
+      <section id="post-footer" className="text-center">
         {lastUpdated && (
           <h3 className="text-xs font-extralight text-gray-600">
             Last updated on {lastUpdated}
