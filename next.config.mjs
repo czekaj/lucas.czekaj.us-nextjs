a54b9e11108b9936b6684df954215b65a0eba1f4
Lucas Czekaj <lukasz@czekaj.us>
2024-02-27 11:47:01 -0800

    Fix error

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const mdx = withMDX({
   extension: /\.mdx?$/,
 });
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const basePath =
   process.env.GITHUB_PAGES === "true" ? __dirname.split("/").pop() : "";
 
