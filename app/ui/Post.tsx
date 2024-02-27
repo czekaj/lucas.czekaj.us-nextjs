6da0e4f6d3f611fadd02a2cbf0f2837afc5302d1
Lucas Czekaj <lukasz@czekaj.us>
2024-02-26 16:35:39 -0800

    Update post style

--- app/ui/Post.tsx
+++ app/ui/Post.tsx
@@ -25,15 +25,15 @@ const Post: NextPage<PostProps> = ({ title, content, date, lastUpdated }) => {
     <article>
       <section id="header">
         <h1 className="text-5xl my-5 text-center">{title}</h1>
-        <h2 className="text-center">{date}</h2>
-        <hr className="mx-4 my-8" />
+        <h2 className="text-center text-gray-400">{date}</h2>
+        <hr className="mx-4 mt-12 mb-16 border-gray-600" />
       </section>
       <section id="body" className="prose m-8 text-gray-200">
         <MDXRemote source={content} options={options as any} />
       </section>
       <section id="footer" className="text-center">
         {lastUpdated && (
-          <h3 className="text-xs font-extralight text-gray-500 italic">
+          <h3 className="text-xs font-extralight text-gray-600">
             Last updated on {lastUpdated}
           </h3>
         )}
