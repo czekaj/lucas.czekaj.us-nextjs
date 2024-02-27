bd331b07e54b3cda89e0c883253d6ba865d8204c
Lucas Czekaj <lukasz@czekaj.us>
2024-02-27 11:48:45 -0800

    Add slash

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const basePath =
-  process.env.GITHUB_PAGES === "true" ? __dirname.split("/").pop() : "";
+  process.env.GITHUB_PAGES === "true" ? "/" + __dirname.split("/").pop() : "";
 
 const nextConfig = {
   ...mdx,
